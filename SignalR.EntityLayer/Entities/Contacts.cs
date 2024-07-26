0b2773c3c663a34db5c121b929c19548877fdc62
AhmetCanArslan <ahmetcanarslan25@gmail.com>
2024-07-27 00:20:14 +0300

    packages are installed

--- SignalR.EntityLayer/Entities/Contacts.cs
+++ SignalR.EntityLayer/Entities/Contacts.cs
@@ -1,12 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace SignalR.EntityLayer.Entities
+﻿namespace SignalR.EntityLayer.Entities
 {
     internal class Contacts
     {
+        public int ContactID { get; set; }
+        public string Location { get; set; }
+        public string Phone { get; set; }
+        public string Mail { get; set; }
+        public string FooterDescription { get; set; }
     }
 }
