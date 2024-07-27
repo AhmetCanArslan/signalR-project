fe10920aa7daea1549ea3086b00ed01fc4685dcb
AhmetCanArslan <ahmetcanarslan25@gmail.com>
2024-07-27 12:49:34 +0300

    data access layers are completed

---
+++ SignalR.DataAccessLayer/EntityFramework/EfContactsDal.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SignalR.DataAccessLayer.Abstract;
+using SignalR.DataAccessLayer.Concrete;
+using SignalR.DataAccessLayer.Repositories;
+using SignalR.EntityLayer.Entities;
+
+namespace SignalR.DataAccessLayer.EntityFramework
+{
+    public class EfContactsDal : GenericRepository<Contacts>, IContactDal
+    {
+        public EfContactsDal(SignalRContext context) : base(context)
+        {
+        }
+    }
+}
