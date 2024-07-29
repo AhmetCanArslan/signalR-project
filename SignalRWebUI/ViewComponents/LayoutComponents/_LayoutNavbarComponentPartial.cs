cefe0d5f5c1492ec6125ba511ed15c14be22131c
AhmetCanArslan <ahmetcanarslan25@gmail.com>
2024-07-29 15:10:53 +0300

    sidebar partial

--- SignalRWebUI/ViewComponents/LayoutComponents/_LayoutNavbarComponentPartial.cs
+++ SignalRWebUI/ViewComponents/LayoutComponents/_LayoutNavbarComponentPartial.cs
@@ -1,8 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Build.ObjectModelRemoting;
 
 namespace SignalRWebUI.ViewComponents.LayoutComponents
 {
-	public class _LayoutSideBarComponentPartial:ViewComponent
+	public class _LayoutNavbarComponentPartial:ViewComponent
 	{
 		public IViewComponentResult Invoke()
 		{
