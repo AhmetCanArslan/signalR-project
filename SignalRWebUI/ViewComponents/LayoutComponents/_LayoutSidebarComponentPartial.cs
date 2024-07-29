a45ce3fd9943977c049fb715c3ba9f27b9bb783e
AhmetCanArslan <ahmetcanarslan25@gmail.com>
2024-07-29 14:58:15 +0300

    admin layout, category an home controller

---
+++ SignalRWebUI/ViewComponents/LayoutComponents/_LayoutSidebarComponentPartial.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Build.ObjectModelRemoting;
+
+namespace SignalRWebUI.ViewComponents.LayoutComponents
+{
+	public class _LayoutSidebarComponentPartial:ViewComponent
+	{
+		public IViewComponentResult Invoke()
+		{
+			return View();
+		}
+	}
+}
