01607bbdb6924ba74451eef0ba89583877ebcebd
AhmetCanArslan <ahmetcanarslan25@gmail.com>
2024-08-02 17:51:13 +0300

    category list api consume

--- SignalRWebUI/Controllers/CategoryController.cs
+++ SignalRWebUI/Controllers/CategoryController.cs
@@ -1,11 +1,28 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using SignalRWebUI.Dtos.CategoryDtos;
 
 namespace SignalRWebUI.Controllers
 {
 	public class CategoryController : Controller
 	{
-		public IActionResult Index()
+		private readonly IHttpClientFactory _clientFactory;
+
+		public CategoryController(IHttpClientFactory clientFactory)
 		{
+			_clientFactory = clientFactory;
+		}
+
+		public async Task<IActionResult> Index()
+		{
+			var client = _clientFactory.CreateClient();
+			var responseMessage = await client.GetAsync("https://localhost:7062/api/Category");
+			if(responseMessage.IsSuccessStatusCode)
+			{
+				var jsonData = await responseMessage.Content.ReadAsStringAsync();
+				var values = JsonConvert.DeserializeObject<List<ResultCategoryDto>>(jsonData);
+				return View(values);
+			}
 			return View();
 		}
 	}
