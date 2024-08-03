de69ea0c365d3fcfd1b5a9bd4e5eb9a4f781dd85
AhmetCanArslan <ahmetcanarslan25@gmail.com>
2024-08-03 14:34:44 +0300

    new category

--- SignalRWebUI/Controllers/CategoryController.cs
+++ SignalRWebUI/Controllers/CategoryController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Text;
+using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
 using SignalRWebUI.Dtos.CategoryDtos;
 
@@ -15,6 +16,7 @@ namespace SignalRWebUI.Controllers
 
 		public async Task<IActionResult> Index()
 		{
+			
 			var client = _clientFactory.CreateClient();
 			var responseMessage = await client.GetAsync("https://localhost:7062/api/Category");
 			if(responseMessage.IsSuccessStatusCode)
@@ -22,6 +24,25 @@ namespace SignalRWebUI.Controllers
 				var jsonData = await responseMessage.Content.ReadAsStringAsync();
 				var values = JsonConvert.DeserializeObject<List<ResultCategoryDto>>(jsonData);
 				return View(values);
+			}
+			return View();
+		}
+		[HttpGet]
+		public IActionResult CreateCategory()
+		{
+			return View();
+		}
+		[HttpPost]	
+		public async Task<IActionResult> CreateCategory(CreateCategoryDto createCategoryDto)
+		{
+			createCategoryDto.Status = true;
+			var client = _clientFactory.CreateClient();
+			var jsonData = JsonConvert.SerializeObject(createCategoryDto);
+			StringContent content = new StringContent(jsonData, Encoding.UTF8, "application/json");
+			var responseMessage = await client.PostAsync("https://localhost:7062/api/Category", content);
+			if(responseMessage.IsSuccessStatusCode)
+			{
+				return RedirectToAction("Index");
 			}
 			return View();
 		}
